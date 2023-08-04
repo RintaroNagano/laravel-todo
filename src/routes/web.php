604b5a3e7540254c7434cfee31e44c6abd1d3b2c
RintaroNagano <rintaro.nagano@gmail.com>
2023-08-05 03:23:24 +0900

    recommit unexpected submodule

---
+++ src/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('todos', 'TodoController@index');
+Route::get('todos/create', 'TodoController@create');
+Route::post('todos', 'TodoController@store');
+Route::get('todos/{id}', 'TodoController@show');
+Route::get('todos/{id}/edit', 'TodoController@edit');
+Route::put('todos/{id}', 'TodoController@update');
+Route::delete('todos/{id}', 'TodoController@destroy');
